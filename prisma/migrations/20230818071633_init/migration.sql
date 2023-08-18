78cc99aeefb69e56d8047dd68e667ba209b2b1f0
Renato Silva <dev.renatosilva@gmail.com>
2023-08-18 04:21:40 -0300

    feat: users and address prisma modeling

---
+++ prisma/migrations/20230818071633_init/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255),
+    "email" VARCHAR(255) NOT NULL,
+    "username" VARCHAR(50) NOT NULL,
+    "telephone" TEXT NOT NULL,
+    "addressId" INTEGER NOT NULL,
+    "avatar" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" SERIAL NOT NULL,
+    "street" VARCHAR(255) NOT NULL,
+    "number" VARCHAR(10) NOT NULL,
+    "complement" VARCHAR(100) NOT NULL,
+    "neighborhood" VARCHAR(255) NOT NULL,
+    "city" VARCHAR(255) NOT NULL,
+    "state" VARCHAR(255) NOT NULL,
+    "country" VARCHAR(255) NOT NULL,
+    "zipcode" VARCHAR(50) NOT NULL,
+    "longitude" VARCHAR(255) NOT NULL,
+    "latitude" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address"("id") ON DELETE CASCADE ON UPDATE CASCADE;
